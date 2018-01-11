17c0f87bd73798a22204b9a27958e67f9c9ebb67
meliveloz <mvelozcampos@gmail.com>
2018-01-11 01:46:45 -0300

    side bar user first

--- assets/js/app.js
+++ assets/js/app.js
@@ -96,3 +96,30 @@ function getMovie(){
     console.log(err);
   })
 }
+
+
+
+/*Funcionalidad Side bar*/
+// Making the side menu work
+
+    $(document).ready(function() {
+
+        $('.menu-toggle').click(function(e){
+
+            var menu = $(this).data('show-dialog');
+
+            $('.' + menu).toggleClass('side-menu-shown');
+
+        });
+
+        $('.side-menu-close').on('click', function () {
+            $('.side-menu-basic').removeClass('side-menu-shown');
+        });
+
+        $('.side-menu-basic a').on('click', function (e) {
+            e.preventDefault();
+
+            alert('You chose option ' + $(this).data('id'));
+        });
+
+    });
